6eb6c460648b1f356302db1c12946e6c5bcc3172
Andrew <andrew.shedov@gmail.com>
2025-08-30 05:16:04 +0300

    commit

--- config example/mongo-checker.config.js
+++ config example/mongo-checker.config.js
@@ -2,7 +2,7 @@ export default {
   uri: "mongodb://127.0.0.1:27017",
   db: "crystalTest",
   collection: "posts",
-  field: "user",
+  field: "createdAt",
   allowDiskUse: true,
-  maxDuplicatesToShow: 200
+  maxDuplicatesToShow: 5
 };
